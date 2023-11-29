b31b86adaca00e666758cbd27e88073d2a2eeedd
Christian Fernández <chr.edg.fer@hotmail.com>
2023-11-29 11:58:29 -0300

    Readme

--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -1,7 +1,12 @@
 const NoPage = () => {
   return (
     <div className="container">
-      <div>404 Error...</div>
+      <div>
+        <p>The page you tried was not found.</p>
+        <p>You may have typed the address incorrectly or you may have used an outdated link.
+If you found a broken link from another site or from our site, please email us.
+</p>
+      </div>
     </div>
   );
 };
