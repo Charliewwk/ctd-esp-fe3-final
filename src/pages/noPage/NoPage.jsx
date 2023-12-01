ab7d521daea02c9cda0b404de0787e681bf2e464
Christian Fernández <chr.edg.fer@hotmail.com>
2023-12-01 01:48:05 -0300

    ThemeSwitch, LanguageSelector, Card, Contact, NoPage, modificaciones varias

--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -3,9 +3,8 @@ const NoPage = () => {
     <div className="container">
       <div>
         <p>The page you tried was not found.</p>
-        <p>You may have typed the address incorrectly or you may have used an outdated link.
-          If you found a broken link from another site or from our site, please email us.
-        </p>
+        <p>You may have typed the address incorrectly or you may have used an outdated link.</p>
+        <p>If you found a broken link from another site or from our site, please email us.</p>
       </div>
     </div>
   );
