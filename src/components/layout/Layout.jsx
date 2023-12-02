85a3c372b834978b0b2853f141f92aaf6388f82a
Christian Fernandez <chr.edg.fer@hotmail.com>
2023-12-02 00:52:02 -0300

    Vercel upload

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
-import "./Layout.css"
+import "./layout.css"
 
 const Layout = () => {
   return (
